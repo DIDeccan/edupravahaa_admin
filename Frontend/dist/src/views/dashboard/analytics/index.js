41640d72c60c46a97d3fda671fb2f668661d16cc
Navyateja-888 <navyateja@dideccanindia.com>
2025-09-01 14:44:07 +0530

    update

--- Frontend/dist/src/views/dashboard/analytics/index.js
+++ Frontend/dist/src/views/dashboard/analytics/index.js
@@ -1,11 +1,22 @@
-import { Col } from "reactstrap"
+
+import { Container, Row, Col } from "reactstrap"
+import ApexBarChart from "./BarChart"
+
 const Dashboard = () => {
-return (
-  <Col>
-  <h4>Dashboard</h4>
-  
-  </Col>
-)
+  return (
+    <Container>
+      <Row className="mb-3">
+        <Col>
+          <h4>Dashboard</h4>
+        </Col>
+      </Row>
+      <Row>
+        <Col md="6">
+          <ApexBarChart />
+        </Col>
+      </Row>
+    </Container>
+  )
 }
 
 export default Dashboard
