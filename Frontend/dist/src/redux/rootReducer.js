b60d27d2231ef22101a723f4bcf143945c7b0a17
Navyateja-888 <navyateja@dideccanindia.com>
2025-09-08 16:57:42 +0530

    WIP: local changes before pulling

--- Frontend/dist/src/redux/rootReducer.js
+++ Frontend/dist/src/redux/rootReducer.js
@@ -2,7 +2,7 @@
 import navbar from './navbar'
 import layout from './layout'
 import auth from './authentication'
-import dashboard from'./DashboardSlice'
+import dashboard from './analyticsSlice'
 import teachers from './teacherSlice'
 
 const rootReducer = {
